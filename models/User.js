c01632ac02d094d236cce7246fccfb937497bb8c
Carlos A. <ciarizam@hotmail.comm>
2024-05-06 20:15:06 -0500

    Super User Implemented 2

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  role: {type: String,enum: ['user', 'admin'],default: 'user'},
+  role: {type: String,enum: ['user', 'admin','superUser'],default: 'user'},
   first_name: { type: String, required: false }, 
   last_name: { type: String, required: false }, 
   email: { type: String, required: true, unique: true },
