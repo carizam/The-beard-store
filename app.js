0055d9d04384c426873accfa7b54dc9dbe614bc9
Carlos A. <ciarizam@hotmail.comm>
2024-06-29 21:13:02 -0500

    Fixed vercel issue

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const cookieParser = require('cookie-parser');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const path = require('path');
 
 // Import routes
 const indexRoutes = require('./routes/index');
@@ -29,6 +32,8 @@ app.use(express.json());
 // Set Handlebars as the view engine
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views')); 
+
 
 // Use body-parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -55,6 +60,20 @@ app.use((req, res, next) => {
 const mongoURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 mongoose.connect(mongoURI, {}).then(() => console.log('MongoDB Atlas connection established')).catch(err => console.error('Mongo connection error', err));
 
+// Swagger setup
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'The Beard Store API',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./routes/*.js'],
+};
+const specs = swaggerJsdoc(options);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+
 // Use routes
 app.use('/', indexRoutes);
 app.use('/', authRoutes);
@@ -64,5 +83,10 @@ app.use('/cart', cartRoutes);
 app.use(notFound); // Catch 404 and forward to error handler
 app.use(errorHandler); // Handle all errors
 
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Start the server
 app.listen(PORT, () => console.log(`The Beard Store server is running on http://localhost:${PORT}`));
+
+module.exports = app;
